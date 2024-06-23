998479d3c6d7dc60fb91dc598334c3d8a3671afd
Lam_Diem_Kieu <kieulam6396@gmail.com>
2024-06-23 18:09:23 +0700

    edit dependence version

--- src/Components/TypycalText/TypicalText.jsx
+++ src/Components/TypycalText/TypicalText.jsx
@@ -1,17 +1,32 @@
-import React from "react";
-import Typical from "react-typical";
-
+import React, { useState, useEffect } from "react";
 import "./TypycalText.scss";
 
 const TypicalText = () => {
+  const [displayText, setDisplayText] = useState("");
+  const texts = [" Lam Diem Kieu", " a Front-end Developer"];
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const currentText = texts[currentIndex];
+      if (displayText.length < currentText.length) {
+        const slicedText = currentText.slice(0, displayText.length + 1);
+        setDisplayText(slicedText);
+      } else if (displayText.length === currentText.length) {
+        clearInterval(intervalId);
+        setTimeout(() => {
+          setCurrentIndex((prevIndex) => (prevIndex + 1) % texts.length);
+          setDisplayText("");
+        }, 2000);
+      }
+    }, 150);
+
+    return () => clearInterval(intervalId);
+  }, [displayText, currentIndex, texts]);
+
   return (
     <h1 className="typicalText">
-      I'm{" "}
-      <Typical
-        steps={[" Lam Diem Kieu", 3000, " a Front-end Developer", 3000]}
-        loop={Infinity}
-        wrapper="span"
-      />
+      I'm<span>{displayText}</span>
     </h1>
   );
 };
